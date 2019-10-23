4acd2c44d8f1597d9b035d02b51aa6a993c5fffa
Sergey <sergetkozlov.2011@yandex.ru>
2019-10-23 22:14:53 +0200

    добавлен контент с разметкой для поста

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,18 +7,26 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MainLayoutComponent } from './shared/components/main-layout/main-layout.component';
 import { HomePageComponent } from './home-page/home-page.component';
 import { PostPageComponent } from './post-page/post-page.component';
+// import {MatToolbarModule} from '@angular/material/toolbar';
+import { PostComponent } from './shared/components/post/post.component';
+import {MatButtonModule, MatCardModule, MatGridListModule, MatToolbarModule} from '@angular/material';
 
 @NgModule({
   declarations: [
     AppComponent,
     MainLayoutComponent,
     HomePageComponent,
-    PostPageComponent
+    PostPageComponent,
+    PostComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatButtonModule,
+    MatGridListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
