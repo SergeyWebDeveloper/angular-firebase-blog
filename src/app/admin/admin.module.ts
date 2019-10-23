e00654a114bcf9c9b3b800836306ad56614c5d8b
Sergey <sergetkozlov.2011@yandex.ru>
2019-10-23 21:44:18 +0200

    добавлены страницы для приложения

---
+++ src/app/admin/admin.module.ts
@@ -0,0 +1,41 @@
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {RouterModule} from '@angular/router';
+import { AdminLayoutComponent } from './shared/components/admin-layout/admin-layout.component';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { DashbordPageComponent } from './dashbord-page/dashbord-page.component';
+import { CreatePageComponent } from './create-page/create-page.component';
+import { EditPageComponent } from './edit-page/edit-page.component';
+
+@NgModule({
+  declarations: [AdminLayoutComponent, LoginPageComponent, DashbordPageComponent, CreatePageComponent, EditPageComponent],
+  imports: [
+    CommonModule,
+    RouterModule.forChild([
+      {
+        path: '', component: AdminLayoutComponent, children: [
+          {
+            path: '', redirectTo: '/admin/login', pathMatch: 'full'
+          },
+          {
+            path: 'login', component: LoginPageComponent
+          },
+          {
+            path: 'dashboard', component: DashbordPageComponent
+          },
+          {
+            path: 'create', component: CreatePageComponent
+          },
+          {
+            path: 'post/:id/edit', component: EditPageComponent
+          }
+        ]
+      }
+    ])
+  ],
+  exports: [RouterModule]
+})
+
+export class AdminModule {
+
+}
